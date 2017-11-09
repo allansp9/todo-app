03dc12e46e8553d827d91ec9cdf9fc9f11ee71ab
Allan Pereira <allansp9@gmail.com>
2017-11-08 22:23:04 -0200

    add item functionality added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+var destination = document.querySelector("#root")
+
+ReactDOM.render(
+    <TodoList />,
+    destination
+);
